f2cc556d99dc356570cda4be34c5c23cd64289ec
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-07-15 17:58:09 +0600

    chnage file

--- server.js
+++ server.js
@@ -1,5 +1,57 @@
 const express = require("express");
-const app = require("./app");
-const dbConnect = require("./Utilities/dbConnect");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 5000;
+const colors = require("colors");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+
+app.use(cors());
+app.use(express.json());
+
+
+
+// import routes
+const dbConnect = require("./Utilities/dbConnect");
+const usersRoute = require('./v1/Routes/users.route');
+const productsRoute = require('./v1/Routes/products.route');
+
+
+
+// connect db
 dbConnect();
+
+
+// declare routes
+app.use('/api/v1/users', usersRoute);
+app.use('/api/v1/products', productsRoute);
+
+
+
+app.get("/", (req, res) => {
+    try {
+        res.send("Welcome to LovBuy e-Commerce Server !");
+    } catch (error) {
+        console.log(error.message);
+    };
+});
+
+app.all("*", (req, res) => {
+    try {
+        res.send("No Routes Found");
+    } catch (error) {
+        console.log(error.message);
+    };
+});
+
+
+app.listen(PORT, () => {
+    try {
+        console.log(`server is successfully running on port ${PORT}!`.red.bold);
+    } catch (error) {
+        console.log(error.message);
+    };
+});
+
+exports = app;
